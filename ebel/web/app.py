ef57afea8cf6e5967ea25a48eeac5cef944e5766
Bruce Schultz <bschultz013@gmail.com>
2021-09-07 10:06:33 +0200

    feat: add host param to app.py

--- ebel/web/app.py
+++ ebel/web/app.py
@@ -10,23 +10,25 @@ application.add_api('openapi.yml')
 CORS(application.app)
 
 
-def run(port: int = 5000, debug_mode: bool = True, open_browser: bool = True):
+def run(host: str = '0.0.0.0', port: int = 5000, debug_mode: bool = True, open_browser: bool = True):
     """Run the API server.
 
     Parameters
     ----------
+    host: str
+        Server or host for the API service.
     port: int
         The defined port to run the API server.
     debug_mode: bool
         Whether bebug mode should be enabled.
     open_browser: bool
-        If True, automatically opens brower to API UI.
+        If True, automatically opens browser to API UI.
     """
-    url = f'http://127.0.0.1:{port}/ui'
+    url = f'http://{host}:{port}/ui'
     if open_browser:
         webbrowser.open(url)
     print(f'Starting web server {url}')
-    application.run(host='0.0.0.0', port=port, debug=debug_mode)
+    application.run(host=host, port=port, debug=debug_mode)
 
 
 if __name__ == '__main__':
