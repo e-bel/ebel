6b6dc34aff842384b6d3a17d082d08f3d8ffd0cf
Bruce Schultz <bschultz013@gmail.com>
2021-10-13 22:44:08 +0200

    Bump version: 1.0.2 → 1.0.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ copyright = '2021, Christian Ebeling, Bruce Schultz'
 author = 'Christian Ebeling, Bruce Schultz'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.2'
+release = '1.0.3'
 
 extensions = [
     'sphinx.ext.autodoc',  # Core Sphinx library for auto html doc generation from docstrings
