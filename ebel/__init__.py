303b666811408d4a1c5ddbd6aa1666fe0b101add
Bruce Schultz <bschultz013@gmail.com>
2022-06-22 09:40:36 +0200

    Bump version: 1.0.8 → 1.0.9

--- ebel/__init__.py
+++ ebel/__init__.py
@@ -7,7 +7,7 @@ from . import cache
 
 from .manager.orientdb.biodbs.bel import Bel
 
-__version__ = '1.0.8'
+__version__ = '1.0.9'
 
 __title__ = 'e(BE:L)'
 __description__ = 'Validation and extension of biomedical knowledge graphs'
