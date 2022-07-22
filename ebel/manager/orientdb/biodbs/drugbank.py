68ae02d079eda96bec0836e8481394e7b9b8993e
Bruce Schultz <bschultz013@gmail.com>
2022-07-22 10:40:12 +0200

    fix: drugbank interaction update only gets human proteins

--- ebel/manager/orientdb/biodbs/drugbank.py
+++ ebel/manager/orientdb/biodbs/drugbank.py
@@ -363,7 +363,7 @@ class DrugBank(odb_meta.Graph):
 
         inserted = 0
 
-        sql = "Select @rid.asString() as rid, uniprot from protein where pure=true and uniprot IS NOT NULL"
+        sql = "Select @rid.asString() as rid, uniprot from protein where pure=true and uniprot IS NOT NULL AND namespace = 'HGNC'"
         for row in tqdm(self.execute(sql), desc=f"Update {self.biodb_name.upper()} interaction."):
             r = row.oRecordData
             protein_rid, uniprot = r['rid'], r['uniprot']
