b1aa716f68f572bb17d4b8bb569ed7de02ff488d
Bruce Schultz <bruce.schultz@scai.fraunhofer.de>
2023-09-08 13:58:23 +0200

    Bump version: 1.0.29 → 1.0.30

--- ebel/__init__.py
+++ ebel/__init__.py
@@ -2,7 +2,7 @@
 from . import cache, constants, errors, parser, transformers
 from .manager.orientdb.biodbs.bel import Bel
 
-__version__ = "1.0.29"
+__version__ = "1.0.30"
 
 __title__ = "e(BE:L)"
 __description__ = "Validation and extension of biomedical knowledge graphs"
