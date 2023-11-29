2da5c09c56c217ee83b274c2d27e459d5bb747c5
Babaiha <66859895+NeginBabaiha@users.noreply.github.com>
2023-11-29 13:06:57 +0100

    update

--- neo4j-import.py
+++ neo4j-import.py
@@ -3,15 +3,20 @@ from pathlib import Path
 from ebel.manager.neo4j.n4j_meta import Neo4jClient
 from ebel.manager.neo4j.bel import Neo4jBel
 
-sherpa_json = "C:/Users/nbabaiha/Documents/GitHub/chatgpt-paper/bel-files/Sherpa/total_sherpa_results_cleaned.bel.json"
-gpt4 = "c:/Users/nbabaiha/Documents/GitHub/chatgpt-paper/bel-files/gpt4/common_triples_5trials/normalized/gpt4_fixed_cleaned.bel.json"
-gpt35 = "c:/Users/nbabaiha/Documents/GitHub/chatgpt-paper/bel-files/gpt3.5-turbo/common_triples_5trials/normalized/gpt35_fixed_cleaned.bel.json"
-tau_KG = "C:/Users/nbabaiha/Documents/GitHub/chatgpt-paper/bel-files/initil-tau-graph/taukg_cleaned.bel.json"
+sherpa = "C:/Users/nbabaiha/Documents/GitHub/chatgpt-paper/neo4j-json-files/abstract/sherpa.json"
+gpt4 = "C:/Users/nbabaiha/Documents/GitHub/chatgpt-paper/neo4j-json-files/abstract/gpt4.json"
+gpt35 = "C:/Users/nbabaiha/Documents/GitHub/chatgpt-paper/neo4j-json-files/abstract/gpt35.json"
+tau = "C:/Users/nbabaiha/Documents/GitHub/chatgpt-paper/neo4j-json-files/abstract/tau_modified.json"
+
+# sherpa = "C:/Users/nbabaiha/Documents/GitHub/chatgpt-paper/neo4j-json-files/full-text/sherpa.json"
+# gpt4 = "C:/Users/nbabaiha/Documents/GitHub/chatgpt-paper/neo4j-json-files/full-text/gpt4.json"
+# gpt35 = "C:/Users/nbabaiha/Documents/GitHub/chatgpt-paper/neo4j-json-files/full-text/gpt35.json"
+# tau = "C:/Users/nbabaiha/Documents/GitHub/chatgpt-paper/neo4j-json-files/full-text/tau_modified.json"
 
 neo = Neo4jClient(uri="bolt://localhost:7687", database="neo4j", user="neo4j", password="12345678")
-#print(neo.session.run("MATCH (n) RETURN n").data())
+#print("test", neo.session.run("MATCH (n) RETURN n").data())
 
 n4jbel = Neo4jBel(client=neo)
-n4jbel.import_json([gpt35,gpt4], update_from_protein2gene=False)
+n4jbel.import_json([sherpa, tau, gpt35, gpt4], update_from_protein2gene=False)
 #n4jbel.import_json([tau_KG, sherpa_json, common_triples_gpt4_json, common_triples_gpt3])
 print("Done")
